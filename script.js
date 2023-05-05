34aa386fa36fccef30f075d98d6175d22c40a42e
Tyree <tyreewhite01102006@gmail.com>
2023-05-05 11:16:00 -0500

    Add lstnr to size grid btn, & fix errors

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const sizeGrid = document.querySelector('.size');
+sizeGrid.addEventListener('click', createGrid);
 
 function createGrid() {
     let size = Number(prompt('Type a number between 1 and 100'));
@@ -6,8 +8,8 @@ function createGrid() {
         alert('Invalid Input!');
         return
     }
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div');
-        grid.appendChild('cell');s
+        grid.appendChild(cell);
     }
 }
